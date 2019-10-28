951ef7270a190ce1a014a3926cc2be98912ed5cd
Mujo Živojević <mujo.zivojevic@edu.fit.ba>
2019-10-28 21:34:31 +0100

    Second commit

---
+++ eRestoran/eRestoran.MobileApp/eRestoran.MobileApp/Models/NarudzbaStavke.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eRestoran.MobileApp.Models
+{
+    public class NarudzbaStavke
+    {
+        public int DetaljiNarudzbeId { get; set; }
+        public int Kolicina { get; set; }
+        public double Cijena { get; set; }
+        public int NarudzbaId { get; set; }
+        public int JeloId { get; set; }
+        public string Jelo { get; set; }
+        public string Model { get; set; }
+    }
+}
---
+++ eRestoran/eRestoran.Model/Requests/NarudzbeStavkeUpsertRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eRestoran.Model.Requests
+{
+    public class NarudzbeStavkeUpsertRequest
+    {
+        public int Kolicina { get; set; }
+        public double Cijena { get; set; }
+        public int NarudzbaId { get; set; }
+        public string Narudzba { get; set; }
+        public string Jelo { get; set; }
+        public int JeloId { get; set; }
+    }
+}
